c3d011b493f7b447b73746c5a446a055086ed9de
William Schneider <vvill@pop-os.localdomain>
2022-01-26 21:44:13 -0300

    start project

---
+++ src/main/java/br/com/drebitocrebito/mainservice/api/v1/HelloWorldController.java
@@ -0,0 +1,20 @@
+package br.com.drebitocrebito.mainservice.api.v1;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+@Slf4j
+@RestController
+@RequestMapping("v1")
+public class HelloWorldController {
+
+    @GetMapping(value = "hello-world", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Mono<String> helloWorld(){
+        log.debug("Request received");
+        return Mono.just("Hello World!");
+    }
+}
